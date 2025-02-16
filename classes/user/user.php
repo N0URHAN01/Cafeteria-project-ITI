ccd88410b6e05ba6e087fe134692507f22fa5a48
init0x1 <abdelrahman.elshaikh7@gmail.com>
2025-02-16 21:43:18 +0200

    implement user auth && get user data with user_id

--- classes/user/user.php
+++ classes/user/user.php
@@ -1,6 +1,8 @@
 <?php
 
 require_once __DIR__ . "/../db/Database.php";
+require_once __DIR__ . "/../../utils/password-utils.php";
+
 
 class User{
     private $db;
@@ -24,6 +26,48 @@ class User{
         }catch(PDOException $e){
             error_log("Database connection error: " . $e->getMessage());
             throw $e;
+        }
+    }
+
+   // get user by user_id
+    public function get_user_by_user_id($user_id){
+        try{
+            $stmt = $this->db->connect()->prepare("SELECT * FROM users WHERE user_id = :user_id");
+            $stmt->bindParam(':user_id', $user_id);
+            $stmt->execute();
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+            return $user;
+        }catch(PDOException $e){
+            error_log("Database connection error: " . $e->getMessage());
+            return false;
+        }
+    }
+   
+   
+   
+    //user authentication
+    public function auth_user($email,$password):string | false{
+        try{
+
+            $stmt = $this->db->connect()->prepare("SELECT user_id, password FROM users WHERE email = :email");
+            $stmt->bindParam(':email', $email);
+            $stmt->execute();
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if(!$user){
+                return false;
+            }
+
+            $hashed_password = hash_password($password,$email);
+
+           if($user['password'] === $hashed_password){
+                return $user['user_id'];
+           }
+           
+           return false;
+        }catch(PDOException $e){
+            error_log("Database connection error: " . $e->getMessage());
+            return false;
         }
     }
 
