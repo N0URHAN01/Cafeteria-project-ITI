35042a0e91793fbfda9468e4d0629874e5a34fdb
init0x1 <abdelrahman.elshaikh7@gmail.com>
2025-02-16 20:26:39 +0200

    update create user method && add file validator

--- utils/validator.php
+++ utils/validator.php
@@ -16,3 +16,20 @@ function validate_posted_data($post_request_data){
     ];
 
 }
+
+
+function validate_file($file, $allowed_extensions) {
+    $errors = [];
+
+    if (!$file || !isset($file['name']) || empty($file['name'])) {
+        $errors["file_error"] = "Please upload an image.";
+        return $errors;
+    }
+
+    $file_extension = pathinfo($file['name'], PATHINFO_EXTENSION);
+    if (!in_array(strtolower($file_extension), $allowed_extensions)) {
+        $errors["file_extension"] = "Not allowed file type of .{$file_extension}";
+    }
+
+    return $errors;
+}
